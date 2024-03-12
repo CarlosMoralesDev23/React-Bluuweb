88a44d075d4b21d9343861f77f218549f722a0ae
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-11 21:46:52 -0300

    fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 
 const App = () =>{
-  return <h1> Hola soy react</h1>
+  return (
+      <>
+          <h1 className="text-center"> Hola soy react</h1>
+          <img src="https://picsum.photos/seed/picsum/200/300" alt="montaña nevada" />
+      </>
+  );
+
 }
 
 export default App
