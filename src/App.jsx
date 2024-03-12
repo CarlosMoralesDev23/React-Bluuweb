f489e1c73d10a00068e31bb85b7ba5bca69b8cf1
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-11 22:05:06 -0300

    componentes con condicionales

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,28 @@ const Button = () => {
     return <button> I am a button </button>;
 };
 
+const OnlineText = () =>{
+    return <h3>Online</h3>
+}
+
+const OfflineText = () => {
+    return <h3>Offline</h3>;
+};
+
 const App = () => {
     const title = `Mi titulo desde una constante`;
     const classTitle = "text-center";
     const pathImg = "https://picsum.photos/seed/picsum/200/300";
+    const user = true
 
     return (
         <Fragment>
             <h1 className={classTitle}>{title}</h1>
             <img src={pathImg} alt="montaña nevada" />
             <Button/>
+            {
+                user ? <OnlineText/> : <OfflineText/>
+            }
         </Fragment>
     );
 };
