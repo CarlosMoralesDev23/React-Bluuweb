09675b6e7cd33ab40a4861a534857e5e9b5811d8
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-11 23:38:45 -0300

    proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Fragment } from "react";
 import montañaNevada from "./assets/images/montaña-nevada.jpg"
+import PropTypes from "prop-types"
 
 
-const Button = () => {
+const Button = ({ text, edad }) => {
 
-    return <button> I am a button </button>;
+    return <button> {text} - {edad} </button>;
+
 };
+
+Button.propTypes = {
+    text : PropTypes.string.isRequired,
+    edad : PropTypes.string.isRequired
+}
 
 const OnlineText = () =>{
     return <h3>Online</h3>
@@ -15,20 +22,29 @@ const OfflineText = () => {
     return <h3>Offline</h3>;
 };
 
+
+const fruits = ['🍉', '🥥', '🍇']
+
 const App = () => {
     const title = `Mi titulo desde una constante`;
     const classTitle = "text-center";
-    const pathImg = montañaNevada;
+    // const pathImg = montañaNevada;
     const user = true
 
     return (
         <Fragment>
             <h1 className={classTitle}>{title}</h1>
-            <img src={pathImg} alt="montaña nevada" />
-            <Button/>
-            {
-                user && <OnlineText/>
-            }
+            <img src={montañaNevada} alt="montaña nevada" />
+            <Button text='botón 2' edad ='10' />
+            <Button text='botón 3' edad ='10' />
+            <Button text='botón 1' edad ='10' />
+
+            {user && <OnlineText />}
+            <ul>
+                {fruits.map((fruit, index) => {
+                    return <li key={index}>{fruit}</li>;
+                })}
+            </ul>
         </Fragment>
     );
 };
