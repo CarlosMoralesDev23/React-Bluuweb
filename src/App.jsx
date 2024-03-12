111a09c7541575d931dc91c5273db0c551150826
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-11 21:49:54 -0300

    fragment 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Fragment } from "react";
+
 
 const App = () =>{
   return (
-      <>
+      <Fragment>
           <h1 className="text-center"> Hola soy react</h1>
           <img src="https://picsum.photos/seed/picsum/200/300" alt="montaña nevada" />
-      </>
+      </Fragment>
   );
 
 }
